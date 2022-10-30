0f86ec0aefb712eb36c6cc333ce54181d520c3f7
gaeun <geun5744@gmail.com>
2022-10-30 16:36:44 +0900

    ADD register.py 로그인 함수 구현

--- register.py
+++ register.py
@@ -1,3 +1,4 @@
+import email
 import pyrebase
 
 firebaseConfig = {
@@ -9,4 +10,19 @@ firebaseConfig = {
   'appId': "1:205816370300:web:d9781abb8849788013e5f9",
   'measurementId': "G-YZ257BPC77"
 }
+
+firebase=pyrebase.initialize_app(firebaseConfig)
+
+#db=firebase.auth()
+auth=firebase.auth()
+#storage=firebase.storage()
+
+#Authentication
+
+#Login
+def Login():
+  email=input("Enter you email")
+  password=input("Enter you password")
+  auth.sign_in_with_email_and_password(email,password)
+  print("Successfully signed in!")
 
