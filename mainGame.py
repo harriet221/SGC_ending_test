a61ee099d44f5ba6f3dcc7bae1cd3f18f57b6a67
harriet221 <potter19@dongguk.edu>
2022-11-06 16:04:39 +0900

    UPDATE mainGame.py 상하 움직임 삭제 빠트린 부분 수정

--- mainGame.py
+++ mainGame.py
@@ -208,10 +208,6 @@ while running:
     key_pressed = pygame.key.get_pressed()
 
     if not player.is_hit:
-        if key_pressed[K_w] or key_pressed[K_UP]:
-            player.moveUp()
-        if key_pressed[K_s] or key_pressed[K_DOWN]:
-            player.moveDown()
         if key_pressed[K_a] or key_pressed[K_LEFT]:
             player.moveLeft()
         if key_pressed[K_d] or key_pressed[K_RIGHT]:
