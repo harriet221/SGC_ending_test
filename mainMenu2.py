cf59701e059c5b16e86c00201e6171ba04690c47
gaeun <geun5744@gmail.com>
2022-11-09 00:06:44 +0900

    ADD mainMenu.py pygame_menu로 메인화면 제작

---
+++ mainMenu2.py
@@ -0,0 +1,86 @@
+import pygame
+from datetime import datetime
+import pygame_menu 
+from os import system
+import button
+import register
+
+pygame.mixer.init()
+
+class Display:
+    w_init = 1/2
+    h_init = 8/9
+    angle = 0
+    help_scale = (0.4,0.4) 
+class Utillization:
+    x = 0
+    y = 1
+
+def start_the_game():
+    import mainGame
+# game variables
+
+gamesound = pygame.mixer.Sound("resource/sound/summer-by-lake-bird-chirping-01.mp3") # example sound
+sound_on = False
+
+pygame.init()
+infoObject = pygame.display.Info()
+size = [int(infoObject.current_w*Display.w_init),int(infoObject.current_h*Display.h_init)]
+screen = pygame.display.set_mode(size,pygame.RESIZABLE)
+
+start_img = pygame.image.load('resource/image/start_btn.png').convert_alpha()
+loginSubmit_button=button.Button(100,300,start_img,0.5)
+
+email_box = button.InputBox(100, 100, 140, 32)
+password_box = button.InputBox(100, 200, 140, 32)
+input_boxes = [email_box,password_box]
+
+def show_mode():
+    menu.clear()
+    menu.add.button('Game Start',start_the_game)
+    menu.add.button('Back', back)
+    menu.add.button('Quit', pygame_menu.events.EXIT)
+
+def back():
+    menu.clear()
+    menu.add.button('Game Start', show_mode)
+    menu.add.button('Help',show_help)
+    menu.add.button('Quit', pygame_menu.events.EXIT)
+
+
+
+def show_help():
+    menu.clear()
+    menu.add.button('Back',back)
+    menu.add.image(image_path='resource/image/help_btn.png', angle=Display.angle, scale=Display.help_scale)
+
+
+
+def login():
+  menu.clear()
+  email=menu.add.text_input("email : ",id='email')
+  password=menu.add.text_input("password : ",password=True,id='password')
+  menu.add.button('Submit',loginButton,email,password)
+
+def loginButton(email,password):
+  print(email.get_value())
+  print(password.get_value())
+
+
+menu_image = pygame_menu.baseimage.BaseImage(image_path='resource/image/store_bg.png',drawing_mode=pygame_menu.baseimage.IMAGE_MODE_FILL)
+mytheme = pygame_menu.themes.THEME_GREEN.copy()
+
+mytheme.background_color = menu_image 
+mytheme.title_bar_style=pygame_menu.widgets.MENUBAR_STYLE_NONE
+
+menu = pygame_menu.Menu('Next Dimension', size[Utillization.x], size[Utillization.y], theme=mytheme)
+menu.add.button('Game Start',start_the_game)
+menu.add.button('Help',show_help)
+menu.add.button('Quit',pygame_menu.events.EXIT)
+menu.add.button("login",login)
+background = pygame.image.load("resource/image/start_btn.png")
+
+
+
+menu.mainloop(screen) 
+pygame.quit()
