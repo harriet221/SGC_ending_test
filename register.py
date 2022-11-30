f669d509670e15bbbe460bbf862f4832daf46793
harriet221 <potter19@dongguk.edu>
2022-11-30 15:45:12 +0900

    FIX requirements.txt && register.py 우분투에서 설치 안되는 오류 수정

--- register.py
+++ register.py
@@ -1,85 +1,89 @@
-from asyncio.windows_events import NULL
 import email
-import firebase_admin 
+import firebase_admin
 from firebase_admin import credentials, firestore
 import pyrebase
 import pygame_menu
 import pygame
 
-cred = credentials.Certificate("./shootinggame-adf10-firebase-adminsdk-mb0cr-86331adc29.json")
-
+cred = credentials.Certificate(
+    "./shootinggame-adf10-firebase-adminsdk-mb0cr-86331adc29.json")
 
 firebaseConfig = {
-  'apiKey': "AIzaSyCTG8WWk89CaPfUPwjNPObOce13gDC-Uro",
-  'authDomain': "shootinggame-adf10.firebaseapp.com",
-  'projectId': "shootinggame-adf10",
-  'storageBucket': "shootinggame-adf10.appspot.com",
-  'messagingSenderId': "205816370300",
-  'appId': "1:205816370300:web:d9781abb8849788013e5f9",
-  'measurementId': "G-YZ257BPC77",
-  'databaseURL':"https://shootinggame-adf10-default-rtdb.firebaseio.com/"
+    'apiKey': "AIzaSyCTG8WWk89CaPfUPwjNPObOce13gDC-Uro",
+    'authDomain': "shootinggame-adf10.firebaseapp.com",
+    'projectId': "shootinggame-adf10",
+    'storageBucket': "shootinggame-adf10.appspot.com",
+    'messagingSenderId': "205816370300",
+    'appId': "1:205816370300:web:d9781abb8849788013e5f9",
+    'measurementId': "G-YZ257BPC77",
+    'databaseURL': "https://shootinggame-adf10-default-rtdb.firebaseio.com/"
 }
 
-firebase_admin.initialize_app(cred,{'databaseURL':"https://shootinggame-adf10-default-rtdb.firebaseio.com/"})
+firebase_admin.initialize_app(
+    cred, {'databaseURL': "https://shootinggame-adf10-default-rtdb.firebaseio.com/"})
 
-firebase=pyrebase.initialize_app(firebaseConfig)
+firebase = pyrebase.initialize_app(firebaseConfig)
 
 
-auth=firebase.auth()
-db=firestore.client()
+auth = firebase.auth()
+db = firestore.client()
 
-#Login
-def Login(email,password):
-  global user
-  user=email
-  print(user)
-  login = 0
-  try:
-    login=auth.sign_in_with_email_and_password(email,password)
-    print("Successfully signed in!")
-  except:
-    print("Invalid user or password. Try again")
-  return login
+# Login
 
-#register
-def register(email,password,confirmPassword):
-  if password==confirmPassword:
+
+def Login(email, password):
+    global user
+    user = email
+    print(user)
+    login = 0
     try:
-      auth.create_user_with_email_and_password(email,password)
-      data={
-        "email":email,
-        "item_apply":"basic",
-        "item":["basic"],
-        "rank":0,
-        "coin":0
-      }
-      db.collection("User").document(email).set(data)
-      return 1
+        login = auth.sign_in_with_email_and_password(email, password)
+        print("Successfully signed in!")
     except:
-      print("Email already exists")
-      return 0
+        print("Invalid user or password. Try again")
+    return login
+
+# register
+
+
+def register(email, password, confirmPassword):
+    if password == confirmPassword:
+        try:
+            auth.create_user_with_email_and_password(email, password)
+            data = {
+                "email": email,
+                "item_apply": "basic",
+                "item": ["basic"],
+                "rank": 0,
+                "coin": 0
+            }
+            db.collection("User").document(email).set(data)
+            return 1
+        except:
+            print("Email already exists")
+            return 0
+
 
 def passwordReset(email):
-  auth.send_password_reset_email(email)
+    auth.send_password_reset_email(email)
 
-#Database
-#Create
-#data={'age':32,'address':'LA','emplyed':False,'name':"Mark"}
-#ref.update({'age':32,'address':'LA','emplyed':False,'name':"Mark"})
+# Database
+# Create
+# data={'age':32,'address':'LA','emplyed':False,'name':"Mark"}
+# ref.update({'age':32,'address':'LA','emplyed':False,'name':"Mark"})
 #db.child("name").push({"company": "google"})
 
 
-#Update
+# Update
 
-#doc_ref=db.collection("User").document("1234sdasd@gmail.com")
-#doc_ref.update({"number":123})
-#Delete
-#db.child("User").child("fjkds").child("age").remove()
+# doc_ref=db.collection("User").document("1234sdasd@gmail.com")
+# doc_ref.update({"number":123})
+# Delete
+# db.child("User").child("fjkds").child("age").remove()
 
-#people=db.child("people").get()
-#for person in people.each():
+# people=db.child("people").get()
+# for person in people.each():
 #  print(person.val())
 #  print(person.key())
 #  if person.val()['name']=='Mark':
 #    db.child('people').child(person.key()).update({'name':'Jane'})
-
